2e50152589f257cfa112d0613122b599a7eaccfc
unknown <noadraw@gmail.com>
2022-08-15 14:33:39 +0300

    button is not rendered

--- Assets/ColonaFill.cs
+++ Assets/ColonaFill.cs
@@ -7,8 +7,11 @@ using UnityEngine.UI;
 public class ColonaFill : MonoBehaviour
 {
     //public GameObject Bottle;
-    public Animator animator;
+    public Animator animator_bottle;
+    public Animator animator_button;
     public float decrase_range = 10;
+    public float Timer = 0;
+    public float Delay = 10;
 
     // Start is called before the first frame update
     void Start()
@@ -25,12 +28,22 @@ public class ColonaFill : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Timer += Time.deltaTime;
+
+        if(Timer > Delay)
+        {
+            animator_button.SetBool("ON",false);
+            Timer = 0;
+        }
     }
 
     void OnClick(PointerEventData data)
     {
-        float current  = animator.GetFloat("BottleLevel");
-        animator.SetFloat("BottleLevel",current-decrase_range);
+        
+        float current  = animator_bottle.GetFloat("BottleLevel");
+        Debug.Log(current);
+        animator_bottle.SetFloat("BottleLevel",current-decrase_range);
+        animator_button.SetBool("ON",true);
+
     }
 }
